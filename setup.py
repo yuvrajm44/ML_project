744569f2b0b45956f513fb3c04007d595c0e0d0e
yuvrajm44 <yuvrajmehta00@gmail.com>
2024-10-21 15:56:56 +0530

    some changes to setup file

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     name='MLproject',                  # Name of your project
     version='0.1',                      # Version of your project
     packages=find_packages(), # Tells where to find the code (in the src folder)
-    package_dir={'': 'src'},           # Says the source code is in the src folder
     install_requires=get_requirments('requirments.txt'),
     author='Yuvraj',                # Your name
     author_email='yuvrajmehta00@gmail.com', # Your email
